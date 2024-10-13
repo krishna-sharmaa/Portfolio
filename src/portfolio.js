86f8ef1a3e4feb1c02a23d26e2c7999641008600
krishna-sharmaa <krishna.sharma@plaksha.edu.in>
2024-10-14 01:03:51 +0530

    New project and resume added

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'Front End Engineer',
   description:
     'I am Krishna Sharma, a Front End Engineer with a passion for building responsive and engaging web applications using modern technologies like React and TypeScript. Currently pursuing a degree in Computer Science and Artificial Intelligence, I’m dedicated to continuous learning and creating impactful digital experiences.',
-  resume: 'https://drive.google.com/file/d/1UbHpbGSMgKnsIzBjaHrSSM-RKoxfD2Mb/view?usp=sharing',
+  resume: 'https://drive.google.com/file/d/1yPxkOKgmiLYb1PO9NpExhY-nZWfrMD72/view?usp=sharing',
   social: {
     linkedin: 'https://www.linkedin.com/in/krishnasharma48',
     github: 'https://github.com/krishna-sharmaa',
@@ -21,7 +21,21 @@ const projects = [
   // projects can be added and removed
   // if there are no projects, the Projects section won't show up
   {
-    name: 'PodNews',
+    name: 'Home Credit - Credit Risk Model Stability',
+    description:
+      'Developed a robust machine learning model to predict client loan defaults with little to no credit history, focusing on stability and accuracy over time.',
+    stack: ['Machine Learning', 'Python', 'Data Science'],
+  
+  },
+  {
+    name: 'Upswipe',
+    description:
+      "Developed a database system using SQL for 'Upswipe', connecting startups and investors through advanced queries, triggers, and views to facilitate efficient matchmaking based on location, industry, and funding stage.",
+    stack: ['SQL'],
+  
+  },
+  {
+    name: 'PodPit',
     description:
       'Led the development of a platform enabling users to generate personalized podcasts based on provided prompts. Utilized Ollama2, Flask2, and text-to-speech technology to create an innovative solution bridging content creation and user engagement.',
     stack: ['Ollama2', 'Flask2', 'Text-to-Speech'],
@@ -33,13 +47,6 @@ const projects = [
       'Collaborated with Prof. Rucha Joshi to develop a tactile-based optics kit for visually impaired students. Designed Arduino-based prototypes and tactile diagrams to facilitate understanding of optical concepts, enhancing accessibility and inclusivity in education.',
     stack: ['Arduino', 'Prototyping', 'Education'],
     
-  },
-  {
-    name: 'Home Credit - Credit Risk Model Stability',
-    description:
-      'Developed a robust machine learning model to predict client loan defaults with little to no credit history, focusing on stability and accuracy over time.',
-    stack: ['Machine Learning', 'Python', 'Data Science'],
-  
   },
 ]
 
